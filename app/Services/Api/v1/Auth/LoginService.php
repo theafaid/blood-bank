cada0fa8e222347d2d8caebceeb5ae4626f81a90
Abdurrahman Faid <abdurrhmanfaid@gmail.com>
2019-12-04 20:54:05 +0200

    Refactor login service

--- app/Services/Api/v1/Auth/LoginService.php
+++ app/Services/Api/v1/Auth/LoginService.php
@@ -7,6 +7,10 @@ use App\Services\Api\v1\ApiResponse;
 
 class LoginService extends ApiResponse
 {
+    /**
+     * @param array $request
+     * @return ClientPrivateResource|\Illuminate\Http\JsonResponse
+     */
     public function handle(array $request)
     {
         if (! $token = $this->clientExists($request)) {
@@ -17,17 +21,29 @@ class LoginService extends ApiResponse
             ]);
         }
 
-        return (new ClientPrivateResource(auth()->user()))
-            ->additional([
-                'meta' => [
-                    'token' => $token,
-                ]
-            ]);
+        return $this->handleClientWithToken($token);
     }
 
 
+    /**
+     * @param $request
+     * @return mixed
+     */
     protected function clientExists($request)
     {
         return auth()->guard('api')->attempt($request);
+    }
+
+    /**
+     * @param $token
+     * @return ClientPrivateResource
+     */
+    protected function handleClientWithToken($token)
+    {
+        return (new ClientPrivateResource(auth()->user()))->additional([
+            'meta' => [
+                'token' => $token,
+            ]
+        ]);
     }
 }
