9bb4be1a451bd1532fe771d22248df7ca9158ceb
Abdurrahman Faid <abdurrhmanfaid@gmail.com>
2019-12-04 17:27:41 +0200

    Setup api routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,9 +65,9 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
+        Route::prefix('api/v1')
              ->middleware('api')
-             ->namespace($this->namespace)
+             ->namespace($this->namespace . '\Api\v1')
              ->group(base_path('routes/api.php'));
     }
 }
